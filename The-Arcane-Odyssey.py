01ea60107e5187ec1bc44096cf91b9e39c2fdfad
Yani Jivkov <114335628+Yani-Jivkov@users.noreply.github.com>
2024-03-27 14:54:30 +0200

    Big Changes

--- The-Arcane-Odyssey.py
+++ The-Arcane-Odyssey.py
@@ -1,23 +1,29 @@
 # The Arcane Odyssey
-
+# Importing libraries
 import cowsay
 import random
 
+# Creating a func for the main game logic
 def Main_Game_Func(total_coins, death_cost, round, level, xp, xp_to_lvl_up, coin_boost_active, wins, looses):
     while True:
 
+        # Adding 1 round
         round += 1
 
+        # Every 10 rounds the death tax increases
         if round % 10 == 0:
             death_cost += 25
 
+        # Picking random possition
         position_of_rock = random.randint(1, 3)
 
         cowsay.tux('Muhaha')
         print(f'\n'      '\n'      '')
         position = input(f'Type the position you are thinking it is going to throw "mid", "left" or "right" ==> ')
 
+        # Chek if you guesssed corectly 
         if dict_helper[position] == position_of_rock:
+            # Chek if you have bought the coin booster from the shop
             if coin_boost_active:
                 print(f'\n'      '\n'      '')
                 print(f'Nice! You are + 150 coins!')
@@ -30,20 +36,24 @@ def Main_Game_Func(total_coins, death_cost, round, level, xp, xp_to_lvl_up, coin
                 total_coins += 100
                 wins += 1
             xp += 10
+            # Chek id you have enough xp to level up
             if xp >= xp_to_lvl_up:
                 level += 1
                 xp -= xp_to_lvl_up
                 xp_to_lvl_up += 20
+                # Every 5th level the program prints the level
                 if level % 5 == 0:
                     print(f'\n'      '')
                     print(f'Reach Level {level}')
 
+        # If you havent guess the possition right
         else:
             print(f'\n'      '\n'      '')
             print(f'Ahh! Mate you just lost {death_cost}')
             total_coins -= 50
             looses += 1
             coin_boost_active = False
+            # Chek if you are out of coins
             if total_coins <= 0:
                 print(f'Game over!')
                 Stats_Func(round, wins, looses, level, total_coins, death_cost)
@@ -51,6 +61,7 @@ def Main_Game_Func(total_coins, death_cost, round, level, xp, xp_to_lvl_up, coin
 
         print(f'\n'      '')
 
+        # While for menu options
         while True:
             print(f'Options:')
             print(f'If you want to fight type "Ready"')
@@ -59,6 +70,7 @@ def Main_Game_Func(total_coins, death_cost, round, level, xp, xp_to_lvl_up, coin
             print(f'If you want to see your stats type "Stats"')
             answer = input(f'>>> ')
 
+            # Check what is the choosen option
             if answer.lower() == 'ready':
                 break
             elif answer.lower() == 'open store':
@@ -75,12 +87,13 @@ def Main_Game_Func(total_coins, death_cost, round, level, xp, xp_to_lvl_up, coin
                 print(f'\n'      '\n'      '')
                 continue
 
-
+# Store logic
 def Store_Func(total_coins, coin_boost_active):
     print(f'\n'      '\n'      '')
     print(f'You have {total_coins} coins')
     print(f'Here are all of the store offerts:')
 
+    # Printing all the items
     for i, x in store_items.items():
         if i == 'Coin Booster':
             print(f'{i} - {x[0]}, {x[1]}')
@@ -88,6 +101,7 @@ def Store_Func(total_coins, coin_boost_active):
         print(f'{i} - {x[0]}, {x[1]}, {x[2]}')
     print(f'\n'      '')
 
+    # While for buying items or coin booster
     while True:
 
         print(f'If you wanna buy an item type the name of the item or if you don\'t want just type "exit"')
@@ -117,6 +131,7 @@ def Store_Func(total_coins, coin_boost_active):
                         break
 
                 elif answer == i and store_items[i][2] == 'Unowned':
+                    # Cheking if you can buy the item
                     if total_coins - 1 >= store_items[i][0]:
                         store_items[i][2] = 'Owned'
                         for x in body_items.keys():
@@ -138,7 +153,9 @@ def Store_Func(total_coins, coin_boost_active):
                 else:
                     continue
 
+# Gear logic
 def Gear_Func():
+    # Printing all of the gear you have
     print(f'\n'      '')
     print(f'That is your gear {name}:')
     for i in body_items.keys():
@@ -148,7 +165,9 @@ def Gear_Func():
     answer = input(f'>>> ')
     print(f'\n'      '\n'      '')
 
+# Stats Func
 def Stats_Func(round, wins, looses, level, total_coins, death_cost):
+    # Printing your variable
     print(f'\n'      '')
     print(f'Thoose are your stats {name}:')
     print(f'Round: {round}')
@@ -173,11 +192,12 @@ def Stats_Func(round, wins, looses, level, total_coins, death_cost):
 #         print(f'\n'      '')
 #         print(f'Reach Level {level}')
 
-
+# Start
 print(f'Welcome to "The Arcane Odyssey" player!')
 print(f'Can you help our team to defeat the big monster?')
 answer = input(f'>>> ')
 
+# Making variables, lists and dictionarities
 total_coins = 300
 death_cost = 50
 level = 1
@@ -202,7 +222,7 @@ print(f'Ok, let\'s begin!')
 print('\n'      '\n'      '')
 
 
-
+# Description
 answer = input(f'Type in "Ready" if you are ==> ')
 print(f'I doesn\'t matter what you say we are starting!')
 print(f'\n'      '\n'      '')
@@ -211,5 +231,6 @@ print(f'You need to type the position you think the rock will fall if you don\'t
 print(f'Every 10th round you will loose with 25 more coins.')
 print(f'Just a reminder that you have {total_coins} coins.')
 print(f'Good luck!')
-                #total_coins, death_cost, round, level, xp, xp_to_lvl_up, coin_boost_active, wins, looses
+
+# Calling the main game func
 Main_Game_Func(total_coins, death_cost, round, level, xp, xp_to_lvl_up, coin_boost_active, wins, looses)
